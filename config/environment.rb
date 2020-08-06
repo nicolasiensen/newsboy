ddae7ccd9526c05e8cd543033f59c91a92570e79
Nícolas Iensen <nicolas.iensen@gmail.com>
2020-08-06 16:30:44 +0200

    Disable the field_with_errors div from Rails forms

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,9 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Disable the field_with_errors div when Rails renders a form with errors.
+# Bootstrap's form validation CSS classes works better this way.
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+  html_tag.html_safe
+end
